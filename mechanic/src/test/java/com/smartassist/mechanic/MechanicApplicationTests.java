2bc9f7bfb3a87f808fc16ca4265c7b34120cff20
Anisa <anisaemin97@gmail.com>
2026-03-09 03:15:51 +0300

    init project structure with microservices

---
+++ mechanic/src/test/java/com/smartassist/mechanic/MechanicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartassist.mechanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
