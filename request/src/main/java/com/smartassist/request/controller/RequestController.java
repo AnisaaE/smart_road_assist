c3015a68f20055ae462decccd0d1a42045398181
Anisa <anisaemin97@gmail.com>
2026-03-20 19:55:59 +0300

    feat: deleteRequest controller success - GREEN (implementation passed)

--- request/src/main/java/com/smartassist/request/controller/RequestController.java
+++ request/src/main/java/com/smartassist/request/controller/RequestController.java
@@ -1,6 +1,7 @@
 package com.smartassist.request.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,12 @@ public class RequestController {
     @PutMapping(REQUEST_BY_ID_PATH)
     public RequestResponse updateRequest(@PathVariable String id, @RequestBody UpdateRequestRequest request) {
         return requestService.updateRequest(id, request);
+    }
+
+    @DeleteMapping(REQUEST_BY_ID_PATH)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRequest(@PathVariable String id) {
+        requestService.deleteRequest(id);
     }
 
     @PostMapping
