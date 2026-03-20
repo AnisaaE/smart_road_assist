e1aec0baf876e5fb6a7b5623fbc53c4403cf5aa2
Anisa <anisaemin97@gmail.com>
2026-03-21 02:08:56 +0300

    feat: redis live state repository config - GREEN

--- mechanic/src/main/java/com/smartassist/mechanic/config/RedisConfig.java
+++ mechanic/src/main/java/com/smartassist/mechanic/config/RedisConfig.java
@@ -4,17 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.smartassist.mechanic.model.MechanicLiveState;
+import com.smartassist.mechanic.repository.MechanicLiveStateRepository;
 
 @Configuration
+@EnableRedisRepositories(basePackageClasses = MechanicLiveStateRepository.class)
 public class RedisConfig {
 
     public static final String MECHANIC_LIVE_STATE_REDIS_TEMPLATE = "mechanicLiveStateRedisTemplate";
 
-    @Bean(MECHANIC_LIVE_STATE_REDIS_TEMPLATE)
+    @Bean({ "redisTemplate", MECHANIC_LIVE_STATE_REDIS_TEMPLATE })
     public RedisTemplate<String, MechanicLiveState> mechanicLiveStateRedisTemplate(
             RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, MechanicLiveState> redisTemplate = new RedisTemplate<>();
