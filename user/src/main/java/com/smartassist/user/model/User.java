2d8ff019c3183c67da3e8f9769f483e759f6066d
ulkuhatip <ulkuhatip@gmail.com>
2026-03-24 21:24:32 +0300

    test: add failing test for user saving (RED state)

---
+++ user/src/main/java/com/smartassist/user/model/User.java
@@ -0,0 +1,5 @@
+package com.smartassist.user.model;
+
+public class User {
+
+}
--- user/src/test/java/com/smartassist/user/UserApplicationTests.java
+++ user/src/test/java/com/smartassist/user/UserApplicationTests.java
@@ -1,13 +1,29 @@
 package com.smartassist.user;
 
+import com.smartassist.user.model.User;
+// import com.smartassist.user.service.UserService; // Burayı henüz açma ki KIRMIZI yansın!
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class UserApplicationTests {
+class UserServiceTest {
 
-	@Test
-	void contextLoads() {
-	}
+    // Senin yazdığın kod (Modelin sağlam olduğunu kanıtlar)
+    @Test
+    void testUserCreation() {
+        User user = new User("1", "Ulku", "ulku@test.com", "USER");
+        assertEquals("Ulku", user.getName());
+    }
 
+    // Benim eklediğim kod (Sistemin 'BEYNİNİN' olmadığını kanıtlar - GERÇEK RED)
+    @Test
+    void shouldCreateUserSuccessfully() {
+        // BURASI KIRMIZI YANMALI (UserService sınıfı henüz oluşturulmadı)
+        UserService userService = new UserService(); 
+        
+        User user = new User("1", "Ulku", "ulku@test.com", "USER");
+        User savedUser = userService.saveUser(user);
+        
+        assertNotNull(savedUser);
+        assertEquals("Ulku", savedUser.getName());
+    }
 }
