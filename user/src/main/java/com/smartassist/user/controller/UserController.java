2677a0671ec0f2d301ba239bfafd7063f2cb9d74
ulkuhatip <ulkuhatip@gmail.com>
2026-03-28 23:09:00 +0300

    refactor: simplify UserController with Lombok and improve null handling (REFACTOR)

--- user/src/main/java/com/smartassist/user/controller/UserController.java
+++ user/src/main/java/com/smartassist/user/controller/UserController.java
@@ -2,35 +2,28 @@ package com.smartassist.user.controller;
 
 import com.smartassist.user.model.User;
 import com.smartassist.user.service.UserService;
+import lombok.RequiredArgsConstructor; // 1. BU IMPORTU EKLE
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users") // RMM Level 2: Kaynak bazlı URI
+@RequestMapping("/users")
+@RequiredArgsConstructor // 2. BU, CONSTRUCTOR'I OTOMATİK OLUŞTURUR
 public class UserController {
 
     private final UserService userService;
 
-        @GetMapping("/{id}") // RMM Level 2 standardı [cite: 58, 59]
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable String id) {
-        User user = userService.getUserById(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
-    // Dependency Injection: Spring bu servisi otomatik bağlayacak
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping // RMM Level 2: Create işlemi için doğru HTTP metodu
+    @PostMapping 
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        // Gerçek mantığı servise devrediyoruz
         User createdUser = userService.createUser(user);
-        
-        // RMM Level 2: Başarılı kayıt sonrası 201 Created dönüyoruz
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
-
-
+    
+    // 3. ELİNLE YAZDIĞIN CONSTRUCTOR'I BURADAN SİLDİK
 }
